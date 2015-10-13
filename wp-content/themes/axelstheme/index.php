55166f1cf9a45ed59138d21c4cfc6dbb5d57bc78
Axel Segebrecht <axel@segebrecht.com>
2015-10-13 22:38:17 +0100

    post thumb added and more

--- wp-content/themes/axelstheme/index.php
+++ wp-content/themes/axelstheme/index.php
@@ -27,10 +27,41 @@
     <meta name="twitter:description" content="A bi-lingual (Ger/Eng) digital web manager for hire, based in Milton Keynes (UK) available to work virtually anywhere." />
     <meta name="twitter:url" content="https://axelsegebrecht.com" />
 	
-	<style amp-custom> body{margin:0}a:active,a:hover{outline:0}code{font-size:1em}input,textarea{color:inherit;font:inherit;margin:0}input[type=submit]{-webkit-appearance:button;cursor:pointer}button::-moz-focus-inner,input::-moz-focus-inner{border:0;padding:0}input{line-height:normal}textarea{overflow:auto}table{border-collapse:collapse;border-spacing:0}td,th{padding:0}.grd{padding-left:1rem;padding-right:1rem}.grd-row{box-sizing:border-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-flex-direction:row;-ms-flex-direction:row;flex-direction:row;-webkit-flex-wrap:wrap;-ms-flex-wrap:wrap;flex-wrap:wrap;max-width:100%}.grd-row-col-1-6{max-width:16.66667%;-webkit-flex-basis:16.66667%;-ms-flex-preferred-size:16.66667%;flex-basis:16.66667%}.grd-row-col-2-6{max-width:33.33333%;-webkit-flex-basis:33.33333%;-ms-flex-preferred-size:33.33333%;flex-basis:33.33333%}.grd-row-col-3-6{max-width:50%;-webkit-flex-basis:50%;-ms-flex-preferred-size:50%;flex-basis:50%}.grd-row-col-4-6{max-width:66.66667%;-webkit-flex-basis:66.66667%;-ms-flex-preferred-size:66.66667%;flex-basis:66.66667%}.grd-row-col-5-6{max-width:83.33333%;-webkit-flex-basis:83.33333%;-ms-flex-preferred-size:83.33333%;flex-basis:83.33333%}.grd-row-col-6{max-width:100%;-webkit-flex-basis:100%;-ms-flex-preferred-size:100%;flex-basis:100%}.grd-row-col-1-6,.grd-row-col-2-6,.grd-row-col-3-6,.grd-row-col-4-6,.grd-row-col-5-6,.grd-row-col-6{-webkit-flex:1;-ms-flex:1;flex:1;-webkit-flex-direction:column;-ms-flex-direction:column;flex-direction:column;box-sizing:border-box}*,:after,:before{box-sizing:border-box}.measure{max-width:48rem;margin:0 auto}a{color:#3498db}a:active,a:focus,a:hover{color:#217dbb}.bg--red{background-color:#e74c3c}.bg--orange{background-color:#f39c12}.bg--blue{background-color:#3498db}.bg--green{background-color:#25ba84}.bg--white{background-color:#fff}.bg--light-gray{background-color:rgba(216,216,216,.99)}.bg--mid-gray{background-color:rgba(144,144,144,.99)}.bg--dark-gray{background-color:rgba(72,72,72,.99)}.bg--off-white{background-color:rgba(250,250,250,.99)}.fnt--red{color:#e74c3c}.fnt--orange{color:#f39c12}.fnt--blue{color:#3498db}.fnt--green{color:#25ba84}.fnt--white{color:#fff}.fnt--light-gray{color:rgba(216,216,216,.99)}.fnt--mid-gray{color:rgba(144,144,144,.99)}.fnt--dark-gray{color:rgba(72,72,72,.99)}.fnt--off-white{color:rgba(250,250,250,.99)}.m0{margin:0}.m1{margin:1rem}.mx1{margin-left:1rem;margin-right:1rem}.my1{margin-top:1rem;margin-bottom:1rem}.m2{margin:2rem}.mx2{margin-left:2rem;margin-right:2rem}.my2{margin-top:2rem;margin-bottom:2rem}.p0{padding:0}.p1{padding:1rem}.px1{padding-left:1rem;padding-right:1rem}.py1{padding-top:1rem;padding-bottom:1rem}.p2{padding:2rem}.px2{padding-left:2rem;padding-right:2rem}.py2{padding-top:2rem;padding-bottom:2rem}html{font-size:12px}@media screen and (min-width:32rem) and (max-width:48rem){html{font-size:15px}}@media screen and (min-width:48rem){html{font-size:16px}}body{font-family:HelveticaNeue-Light,"Helvetica Neue Light","Helvetica Neue",Helvetica,Arial,sans-serif;font-weight:400;line-height:1.85;color:#333}p{font-size:1rem;margin-bottom:1.3rem}.h1,.h2,.h3,.h4,h1,h2,h3,h4{margin:1.414rem 0 .5rem;font-weight:inherit;line-height:1.42}.h1,h1{margin-top:0;font-size:3.998rem}.h2,h2{font-size:2.827rem}.h3,h3{font-size:1.999rem}.h4,h4{font-size:1.414rem}.h5,table th{font-size:1.121rem}.h6,h6{font-size:.88rem}.btn--s,.small,small{font-size:.707em}code{font-family:Monaco,"Lucida Console",Courier,monospace}.italic{font-style:italic}.thin{font-weight:100}.bold{font-weight:700}.caps,table th{font-variant:small-caps}.muted{opacity:.7}.txt--center{text-align:center}.txt--right{text-align:right}.txt--left,table th{text-align:left}.oflow-y--scroll{overflow-y:scroll}.w100--s{width:100%;display:block}@media screen and (min-width:32rem){.w100--s{width:auto;display:inline-block}}img,textarea{max-width:100%}.btn,.btn--blue,.btn--gray,.btn--green,.btn--link,.btn--red{border-radius:3px}.brdr--light-gray{border:thin solid rgba(216,216,216,.99)}.brdr--mid-gray{border:thin solid rgba(144,144,144,.99)}.brdr--dark-gray{border:thin solid rgba(72,72,72,.99)}.brdr--off-white{border:thin solid rgba(250,250,250,.99)}.btn,.btn--blue,.btn--gray,.btn--green,.btn--link,.btn--red{padding:.5rem 1rem;border:2px solid #ccc;color:#333;text-decoration:none;text-align:center}.btn:active,.btn:focus,.btn:hover{background-color:rgba(0,0,0,.05);color:#000}.btn--link{border-color:transparent;color:#3498db}.btn--link:hover{background-color:transparent;color:#1d6fa5}.btn--blue{border-color:#2792d9;background-color:#3498db;color:#fff}.btn--blue:active,.btn--blue:focus,.btn--blue:hover{color:#fff;background-color:#217dbb}.btn--green{border-color:#22a977;background-color:#25ba84;color:#fff}.btn--green:active,.btn--green:focus,.btn--green:hover{color:#fff;background-color:#1d8f65}.btn--gray{border-color:rgba(59,59,59,.99);background-color:rgba(72,72,72,.99);color:#fff}.btn--gray:active,.btn--gray:focus,.btn--gray:hover{color:#fff;background-color:rgba(46,46,46,.99)}.btn--red{border-color:#e43525;background-color:#e74c3c;color:#fff}.btn--red:active,.btn--red:focus,.btn--red:hover{color:#fff;background-color:#d62a1a}.btn--s{padding:.5rem}.list--unstyled{list-style-type:none}input,textarea{padding:.5rem;margin-bottom:.5rem;display:block;width:100%;box-shadow:none;border:thin solid rgba(216,216,216,.99)}input[type=submit]{margin-top:.85rem}.media{display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-align-items:flex-start;-ms-flex-align:start;align-items:flex-start}.media .media-figure{margin-right:1em}.media .media-body{-webkit-flex:1 0 0;-ms-flex:1 0 0;flex:1 0 0}table>thead{border-bottom:thin solid rgba(250,250,250,.99)}table td,table th{padding:.25rem;word-wrap:none;line-height:1}
-	
-	.burger {vertical-align:middle;margin-top:-2px;}
+	<link href="https://fonts.googleapis.com/css?family=Pacifico|Source+Serif+Pro:400,600,700" rel="stylesheet" type="text/css">
 
+	<style amp-custom> body{margin:0}a:active,a:hover{outline:0}code{font-size:1em}input,textarea{color:inherit;font:inherit;margin:0}input[type=submit]{-webkit-appearance:button;cursor:pointer}button::-moz-focus-inner,input::-moz-focus-inner{border:0;padding:0}input{line-height:normal}textarea{overflow:auto}table{border-collapse:collapse;border-spacing:0}td,th{padding:0}.grd{padding-left:1rem;padding-right:1rem}.grd-row{box-sizing:border-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-flex-direction:row;-ms-flex-direction:row;flex-direction:row;-webkit-flex-wrap:wrap;-ms-flex-wrap:wrap;flex-wrap:wrap;max-width:100%}.grd-row-col-1-6{max-width:16.66667%;-webkit-flex-basis:16.66667%;-ms-flex-preferred-size:16.66667%;flex-basis:16.66667%}.grd-row-col-2-6{max-width:33.33333%;-webkit-flex-basis:33.33333%;-ms-flex-preferred-size:33.33333%;flex-basis:33.33333%}.grd-row-col-3-6{max-width:50%;-webkit-flex-basis:50%;-ms-flex-preferred-size:50%;flex-basis:50%}.grd-row-col-4-6{max-width:66.66667%;-webkit-flex-basis:66.66667%;-ms-flex-preferred-size:66.66667%;flex-basis:66.66667%}.grd-row-col-5-6{max-width:83.33333%;-webkit-flex-basis:83.33333%;-ms-flex-preferred-size:83.33333%;flex-basis:83.33333%}.grd-row-col-6{max-width:100%;-webkit-flex-basis:100%;-ms-flex-preferred-size:100%;flex-basis:100%}.grd-row-col-1-6,.grd-row-col-2-6,.grd-row-col-3-6,.grd-row-col-4-6,.grd-row-col-5-6,.grd-row-col-6{-webkit-flex:1;-ms-flex:1;flex:1;-webkit-flex-direction:column;-ms-flex-direction:column;flex-direction:column;box-sizing:border-box}*,:after,:before{box-sizing:border-box}.measure{max-width:48rem;margin:0 auto}a{color:#3498db}a:active,a:focus,a:hover{color:#217dbb}.bg--red{background-color:#e74c3c}.bg--orange{background-color:#f39c12}.bg--blue{background-color:#3498db}.bg--green{background-color:#25ba84}.bg--white{background-color:#fff}.bg--light-gray{background-color:rgba(216,216,216,.99)}.bg--mid-gray{background-color:rgba(144,144,144,.99)}.bg--dark-gray{background-color:rgba(72,72,72,.99)}.bg--off-white{background-color:rgba(250,250,250,.99)}.fnt--red{color:#e74c3c}.fnt--orange{color:#f39c12}.fnt--blue{color:#3498db}.fnt--green{color:#25ba84}.fnt--white{color:#fff}.fnt--light-gray{color:rgba(216,216,216,.99)}.fnt--mid-gray{color:rgba(144,144,144,.99)}.fnt--dark-gray{color:rgba(72,72,72,.99)}.fnt--off-white{color:rgba(250,250,250,.99)}.m0{margin:0}.m1{margin:1rem}.mx1{margin-left:1rem;margin-right:1rem}.my1{margin-top:1rem;margin-bottom:1rem}.m2{margin:2rem}.mx2{margin-left:2rem;margin-right:2rem}.my2{margin-top:2rem;margin-bottom:2rem}.p0{padding:0}.p1{padding:1rem}.px1{padding-left:1rem;padding-right:1rem}.py1{padding-top:1rem;padding-bottom:1rem}.p2{padding:2rem}.px2{padding-left:2rem;padding-right:2rem}.py2{padding-top:2rem;padding-bottom:2rem}html{font-size:12px}@media screen and (min-width:32rem) and (max-width:48rem){html{font-size:15px}}@media screen and (min-width:48rem){html{font-size:16px}}body{font-family:HelveticaNeue-Light,"Helvetica Neue Light","Helvetica Neue",Helvetica,Arial,sans-serif;font-weight:400;line-height:1.85;color:#333}p{font-size:1.2rem;margin-bottom:1.3rem}.h1,.h2,.h3,.h4,h1,h2,h3,h4{margin:1.414rem 0 .5rem;font-weight:inherit;line-height:1.42}.h1,h1{margin-top:0;font-size:3.998rem}.h2,h2{font-size:2.827rem}.h3,h3{font-size:1.999rem}.h4,h4{font-size:1.414rem}.h5,table th{font-size:1.121rem}.h6,h6{font-size:.88rem}.btn--s,.small,small{font-size:.707em}code{font-family:Monaco,"Lucida Console",Courier,monospace}.italic{font-style:italic}.thin{font-weight:100}.bold{font-weight:700}.caps,table th{font-variant:small-caps}.muted{opacity:.7}.txt--center{text-align:center}.txt--right{text-align:right}.txt--left,table th{text-align:left}.oflow-y--scroll{overflow-y:scroll}.w100--s{width:100%;display:block}@media screen and (min-width:32rem){.w100--s{width:auto;display:inline-block}}img,textarea{max-width:100%}.btn,.btn--blue,.btn--gray,.btn--green,.btn--link,.btn--red{border-radius:3px}.brdr--light-gray{border:thin solid rgba(216,216,216,.99)}.brdr--mid-gray{border:thin solid rgba(144,144,144,.99)}.brdr--dark-gray{border:thin solid rgba(72,72,72,.99)}.brdr--off-white{border:thin solid rgba(250,250,250,.99)}.btn,.btn--blue,.btn--gray,.btn--green,.btn--link,.btn--red{padding:.5rem 1rem;border:2px solid #ccc;color:#333;text-decoration:none;text-align:center}.btn:active,.btn:focus,.btn:hover{background-color:rgba(0,0,0,.05);color:#000}.btn--link{border-color:transparent;color:#3498db}.btn--link:hover{background-color:transparent;color:#1d6fa5}.btn--blue{border-color:#2792d9;background-color:#3498db;color:#fff}.btn--blue:active,.btn--blue:focus,.btn--blue:hover{color:#fff;background-color:#217dbb}.btn--green{border-color:#22a977;background-color:#25ba84;color:#fff}.btn--green:active,.btn--green:focus,.btn--green:hover{color:#fff;background-color:#1d8f65}.btn--gray{border-color:rgba(59,59,59,.99);background-color:rgba(72,72,72,.99);color:#fff}.btn--gray:active,.btn--gray:focus,.btn--gray:hover{color:#fff;background-color:rgba(46,46,46,.99)}.btn--red{border-color:#e43525;background-color:#e74c3c;color:#fff}.btn--red:active,.btn--red:focus,.btn--red:hover{color:#fff;background-color:#d62a1a}.btn--s{padding:.5rem}.list--unstyled{list-style-type:none}input,textarea{padding:.5rem;margin-bottom:.5rem;display:block;width:100%;box-shadow:none;border:thin solid rgba(216,216,216,.99)}input[type=submit]{margin-top:.85rem}.media{display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-align-items:flex-start;-ms-flex-align:start;align-items:flex-start}.media .media-figure{margin-right:1em}.media .media-body{-webkit-flex:1 0 0;-ms-flex:1 0 0;flex:1 0 0}table>thead{border-bottom:thin solid rgba(250,250,250,.99)}table td,table th{padding:.25rem;word-wrap:none;line-height:1}
+
+	a {text-decoration: none;color: #000;box-shadow: inset 0 -4px 0 #c0e6f7;}
+	.btn--blue, .btn {box-shadow:0 0;}
+	
+	body {font-family: 'Source Serif Pro', serif;}
+	.brand-logo {font-family: 'Pacifico', sans-serif;font-size:1.25rem;}
+	.brand-logo a {text-decoration:none;color:#c00;box-shadow: 0 0;}
+	
+	.menu {padding-top:0.5rem;}
+	.menu li {display: inline;list-style-type: none;padding-right: 0.8rem;}
+	
+	.home {
+		background: url("<?php if ( has_post_thumbnail() ) { 
+			// get the image URL only
+			$thumb_id = get_post_thumbnail_id();
+			$thumb_url = wp_get_attachment_image_src($thumb_id,'full', true);
+			echo $thumb_url[0];
+			 } else { 
+		    // if nothing is set-up let's use a generic fall-back image from our theme
+			echo get_stylesheet_directory_uri(); ?>/photo-1431578500526-4d9613015464-1610.jpg<?php } ?>"); 
+		background-size: cover;
+	    background-repeat: no-repeat;
+		background-position: left center;
+	}
+	.summary p, .pagetitle {
+		background: #F9E437;
+		display: inline;
+		box-shadow: 0.6rem 0 0 #F9E437, -0.6rem 0 0 #F9E437;
+	}
+	.summary p {font-size: 1.4rem;}
+	.summary .cta {display:block;margin-top: 1rem;}
+	
 	
     </style>
     <script type="application/ld+json">
@@ -44,73 +75,91 @@
       "datePublished": "2015-02-05T08:00:00+08:00"
     }
     </script>
-    <script async custom-element="amp-carousel" src="https://cdn.ampproject.org/v0/amp-carousel-0.1.js"></script>
+	<script async custom-element="amp-iframe" src="https://cdn.ampproject.org/v0/amp-iframe-0.1.js"></script>
     <style>body {opacity: 0}</style><noscript><style>body {opacity: 1}</style></noscript>
     <script async src="https://cdn.ampproject.org/v0.js"></script>
   </head>
   <body>
-	  
+
+<amp-iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NVFRFQ" height="0" width="0" style="display:none;visibility:hidden"></amp-iframe>
+
+	<header class="grd bg--off-white">		
+		<div class="grd-row">
+	    	<div class="grd-row-col-2-6 py1 brand-logo">
+				<a href="#" title="Axel Segebrecht's website">Axel Segebrecht</a>
+			</div>
+			<div class="grd-row-col-4-6 nav txt--right">
+				<nav>
+					<ul class="menu">
+						<li><a href="">Home</a></li>
+						<li><a href="">About</a></li>
+						<li><a href="">Contact</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+	
+	<main role="main">	  
 	<?php if (have_posts()) : ?>
 	<?php while (have_posts()) : the_post(); ?>
-	  
-	<div class="py2">
-		<div class="measure p2 txt--center">
-			<h1 id="post-<?php the_ID(); ?>" class="h2">
-				<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
-					<?php the_title(); ?><br />
-				</a>
-				<small class="small muted"><?php the_excerpt(); ?></small>
-			</h1>
-	        <div class="my2">
-				<a href="#navbar-collapse" class="navbar-toggle collapsed btn" title="View navigation menu"><amp-img class="burger" src="<?php echo get_stylesheet_directory_uri(); ?>/burger.svg" width="16" height="16"></amp-img> Menu</a>
-
-          <div id="navbar-collapse" class="navbar-collapse collapse">
-            <a class="navbar-collapse-close" href="#navbar">Close menu</a>
-            <ul class="nav navbar-nav">
-              <li class="active"><a href="#">Home</a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Contact</a></li>
-            </ul>            
-          </div><!--/.nav-collapse -->
-				
-				<a href="#article" class="btn" title="Jump to read the article"><strong>Read article &#8594;</strong></a>
-	        </div>
-		</div>
-	</div>
+		<article>  
+			<div class="py2 home">
+				<div class="measure p2 txt--center">
+					<h1 id="post-<?php the_ID(); ?>" class="h2 pagetitle">
+						<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
+							<?php the_title(); ?><br />
+						</a>
+						
+					</h1>
+			        <div class="my2 summary">
+							<?php the_excerpt(); ?>
+						<div class="cta">
+							<a href="#article" class="btn--blue" title="Jump to read the article">
+								<strong>Read article &#8594;</strong>
+							</a>
+						</div>
+			        </div>
+				</div>
+			</div>
 	
-	<div class="py2 bg--off-white">
-		<div class="measure p2 entry" id="article">
+			<div class="py2 bg--off-white">
+				<div class="measure p2 entry" id="article">
 	
-			<?php the_content('Read the rest of this entry &raquo;'); ?>
+					<?php the_content('Read the rest of this entry &raquo;'); ?>
 		 
-			<p class="postmetadata small">
-				<?php edit_post_link('Edit','','<strong> &#8226; </strong>'); ?>
-				Posted on <?php the_time('F jS, Y') ?> and filed under <?php the_category(', ') ?>
-			</p>
-			<p class="postmetadata">
-				<?php comments_popup_link('Send your comments »', 'View comment »', 'View % comments »'); ?>
-			</p>
+					<p class="postmetadata small">
+						<?php edit_post_link('Edit','','<strong> &#8226; </strong>'); ?>
+						Posted on <?php the_time('F jS, Y') ?> at <?php the_time() ?> and filed under <?php the_category(', ') ?>
+					</p>	
+					<p class="postmetadata tags">
+						<?php the_tags( 'Find related content: ', ' • ', '<br />' ); ?>
+					</p>
+					<p class="postmetadata">
+						<?php comments_popup_link('Send your comments »', 'View comment »', 'View % comments »'); ?>
+					</p>
 
-			<?php endwhile; ?>
+					<?php endwhile; ?>
 
-			<?php else : ?>
-			<h2 class="txt--center">Nothing found</h2>
-				<p class="txt--center">
-					<?php _e("Sorry, but you are looking for something that isn't here."); ?><br />
-					<a href="#!" class="w100--s my1 btn--red" title="Furtive button example">&#8592; Return home</a><br />
-					<amp-img src="<?php echo get_stylesheet_directory_uri(); ?>/404.jpg" width="300" height="300"></amp-img>
-				</p>
+					<?php else : ?>
+					<h2 class="txt--center">Nothing found</h2>
+						<p class="txt--center">
+							<?php _e("Sorry, but you are looking for something that isn't here."); ?><br />
+							<a href="#!" class="w100--s my1 btn--red" title="Furtive button example">&#8592; Return home</a><br />
+							<amp-img src="<?php echo get_stylesheet_directory_uri(); ?>/404.jpg" width="300" height="300"></amp-img>
+						</p>
 
-			<?php endif; ?>
+					<?php endif; ?>
 		  
-		</div>
-	</div>
+				</div>
+			</div>
+		</article>
+	</main>
 
 	<footer class="txt--center p2">
 		<p class="small p2">
-			Crafted with &lt;3 by <a href="http://johnotander.com" title="John Otander's website">John Otander</a>
-			(<a href="https://twitter.com/4lpine" title="John Otander on Twitter (@4lpine)">@4lpine</a>). <br>
-			&lt;/&gt; available on <a href="https://github.com/johnotander/furtive" title="Furtive source code">Github</a>.
+			Wordpress theme by <a href="http://axelsegebrecht.com" title="Axel Segebrecht's website">Axel Segebrecht</a> (<a href="https://twitter.com/axelsegebrecht" target="_blank" title="Follow or tweet to Axel on Twitter">@axelsegebrecht</a>), based on <a href="https://github.com/johnotander/furtive" target="_blank" title="Furtive repository on github">furtive</a> by <a href="http://johnotander.com" title="John Otander's website">John Otander</a>.<br>
+			<a href="https://wordpress.org" target="_blank" title="Wordpress website">Wordpress</a> theme &lt;/&gt; available on <a href="https://github.com/axelsegebrecht/axelstheme" title="Axel Theme's source code on github" target="_blank">Github</a>. Proudly hosted by <a href="https://wpengine.com" target="_blank" title="WP Engine website">WP Engine</a>.
 		</p>
 	</footer>
 
